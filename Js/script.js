4c75d46d341a3cbc2096b65fa93a7da7afc9b72b
Aditya chandel <adityachandel371@gmail.com>
2025-03-28 21:53:23 +0530

    Added js for navbar

--- Js/script.js
+++ Js/script.js
@@ -5,8 +5,10 @@ const nav = document.querySelector("nav"); // Selects the first <nav> element
 window.addEventListener("scroll", () => {
     if (window.scrollY > 100) {
         nav.style.background = "#021526";
+        nav.style.borderColor = "#d3d7de";
     } else {
         nav.style.background = "transparent"; // Reset background when scrolled back up
+        nav.style.borderColor = "transparent"; // Reset color of border when scrolled back up
     }
 });
 
@@ -19,13 +21,10 @@ const sr = ScrollReveal({
     reset: false
 });
 
-// home page
-
 // home section
 sr.reveal('.content', { delay: 50, origin: 'top' });
 
 // intro section
-
 sr.reveal('.info-text', { delay: 50, origin: 'left' });
 sr.reveal('.info-image', { delay: 50, origin: 'right' });
 
